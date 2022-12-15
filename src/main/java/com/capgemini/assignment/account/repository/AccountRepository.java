771075036b9e9f69002c7ecd6a879a5908480245
Osmane Chayma <chaymaaosmane@hmail.com>
2022-12-15 21:47:38 +0100

    Adding FindByCustomer to get all acount of the customer

--- src/main/java/com/capgemini/assignment/account/repository/AccountRepository.java
+++ src/main/java/com/capgemini/assignment/account/repository/AccountRepository.java
@@ -1,10 +1,13 @@
 package com.capgemini.assignment.account.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.capgemini.assignment.account.entity.Account;
+import com.capgemini.assignment.account.entity.Customer;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, String> {
-
+	public List<Account> findByCustomer(Customer customer);
 }
